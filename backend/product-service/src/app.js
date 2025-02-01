9a27cd51d93ace5e78ceb697505dfa5fe9937c52
Sagar Shirgaonkar <sagarshirgaonkar59511@gmail.com>
2025-02-01 14:53:38 +0530

    product service added

--- backend/product-service/src/app.js
+++ backend/product-service/src/app.js
@@ -1,7 +1,6 @@
 import express from "express";
 import cors from "cors";
-import homeRouter from "./routes/home.routes.js";
-import paymentRouter from "./routes/payment.routes.js";
+import homeRouter from "./routes/home.routes.js"
 
 const app = express();
 
@@ -13,7 +12,6 @@ app.use(cors({
 app.use(express.urlencoded({extended:true,limit:"16kb"}));
 app.use(express.json({limit:"16kb"}));
 
-app.use("/",homeRouter);
-app.use("/pay",paymentRouter);
+app.use("/",homeRouter)
 
 export {app}
